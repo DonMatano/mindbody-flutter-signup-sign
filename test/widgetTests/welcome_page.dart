52f9aa5a0947b836937f98d2dd2b4f0c558c1a7b
Mwakima Matano <mwakimamatano@gmail.com>
2019-06-01 21:39:16 +0300

    Remove some tests. Will return later

---
+++ test/widgetTests/welcome_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mindblown_sign_in_up_flutter/pages/welcome.page.dart';
+
+
+// void main() {
+//   testWidgets('Stack Widget is loaded', (WidgetTester tester) async {
+//     await tester.pumpWidget(WelcomePage());
+//     final stackWidgetFinder = find.byWidget(Stack())
+//   });
+// }
