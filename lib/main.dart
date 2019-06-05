0391c42259599c1039c5a4f9acf05e041f815bf2
Mwakima Matano <mwakimamatano@gmail.com>
2019-06-05 20:44:12 +0300

    Add App theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mindblown_sign_in_up_flutter/pages/login.page.dart';
 import 'package:mindblown_sign_in_up_flutter/pages/welcome.page.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: 
-  Colors.deepOrangeAccent));
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.deepOrangeAccent));
   runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: WelcomePage.routeName,
+      routes: {
+        WelcomePage.routeName: (context) => WelcomePage(),
+        LoginPage.routeName: (context) => LoginPage(),
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: 'Lato',
+        primarySwatch: Colors.deepOrange,
         textTheme: TextTheme(
-          headline: TextStyle(color: Colors.white, ),
-          body1: TextStyle(color: Colors.white)
+          headline: TextStyle(
+            color: Colors.white,
+          ),
+          body1: TextStyle(color: Colors.white),
+        ),
+        iconTheme: IconThemeData(color: Colors.white),
+        inputDecorationTheme: InputDecorationTheme(
+          labelStyle: TextStyle(color: Colors.white, fontSize: 12),
+          contentPadding: const EdgeInsets.all(8),
+          
         )
       ),
-      home: WelcomePage()
     );
   }
 }
