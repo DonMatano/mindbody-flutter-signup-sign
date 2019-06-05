f2b5d5777d3a41988c53c3fc4a98404966a63c9f
Mwakima Matano <mwakimamatano@gmail.com>
2019-06-05 20:45:31 +0300

    Add test to check if login button take user to login page

--- test/widgetTests/main.dart
+++ test/widgetTests/main.dart
@@ -1,30 +1,33 @@
-// // This is a basic Flutter widget test.
-// //
-// // To perform an interaction with a widget in your test, use the WidgetTester
-// // utility that Flutter provides. For example, you can send tap and scroll
-// // gestures. You can also use WidgetTester to find child widgets in the widget
-// // tree, read text, and verify that the values of widget properties are correct.
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:mindblown_sign_in_up_flutter/main.dart';
+import 'package:mindblown_sign_in_up_flutter/main.dart';
+import 'package:mindblown_sign_in_up_flutter/pages/login.page.dart';
+import 'package:mindblown_sign_in_up_flutter/pages/welcome.page.dart';
 
-// void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(MyApp());
+void main() {
+  testWidgets('Login button takes user to login page', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+    final welcomePageFinder = find.byType(WelcomePage);
+    final loginPageFinder = find.byType(LoginPage);
+    final loginButtonFinder = find.descendant(of: welcomePageFinder, matching: find.byKey(Key('Login button')));
 
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
+    expect(welcomePageFinder, findsOneWidget);
+    expect(loginPageFinder, findsNothing);
+    expect(loginButtonFinder, findsOneWidget);
 
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
+    await tester.tap(loginButtonFinder);
+    await tester.pumpAndSettle();
 
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
-// }
+    expect(loginPageFinder, findsOneWidget);
+    expect(welcomePageFinder, findsNothing);
+  });
+}
