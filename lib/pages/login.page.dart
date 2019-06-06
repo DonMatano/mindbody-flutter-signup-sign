631aa2476d404300863e13627daadb05d2305e6e
Mwakima Matano <mwakimamatano@gmail.com>
2019-06-06 09:11:18 +0300

    Add login page UI

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mindblown_sign_in_up_flutter/components/background.component.dart';
+import 'package:mindblown_sign_in_up_flutter/presentation/customIcons.dart';
 
 class LoginPage extends StatelessWidget {
   static const routeName = '/login';
@@ -23,35 +24,114 @@ class LoginPage extends StatelessWidget {
               BackgroundImage('images/orange wavy.jpg'),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                      height: 100.0,
-                    ),
-                    Text(
-                      'Welcome back',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                    ),
-                    TextFormField(
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.email),
-                        labelText: 'Email Address',
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 50.0,
                       ),
-                    ),
-                    TextFormField(
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.email),
-                        labelText: 'Email Address',
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w500),
                       ),
-                    ),
-                  ],
+                      TextFormField(
+                        style: TextStyle(color: Colors.white),
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: Colors.white,
+                            size: 18,
+                          ),
+                          labelText: 'Email Address',
+                        ),
+                      ),
+                      TextFormField(
+                        style: TextStyle(color: Colors.white),
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.security,
+                            color: Colors.white,
+                            size: 18,
+                          ),
+                          labelText: 'Password',
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: FlatButton(
+                          child: Text(
+                            'Forgot Password ?',
+                            style:
+                                TextStyle(decoration: TextDecoration.underline),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: OutlineButton(
+                          key: Key('Login button'),
+                          child: Text('LOG IN',
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  letterSpacing: 1,
+                                  wordSpacing: 2)),
+                          textColor: Colors.white,
+                          padding: EdgeInsets.all(12.0),
+                          borderSide: BorderSide(color: Colors.white),
+                          onPressed: () {},
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 16),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Divider(
+                                color: Colors.white,
+                              ),
+                            ),
+                            Container(
+                                margin: EdgeInsets.symmetric(horizontal: 10),
+                                child: Text('or')),
+                            Expanded(
+                              child: Divider(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      iconButtonWidget(CustomIcons.facebook_squared, 'LOGIN WITH FACEBOOK'),
+                      iconButtonWidget(CustomIcons.google, 'LOGIN WITH GOOGLE'),
+                    ],
+                  ),
                 ),
               )
             ],
           ),
         ));
+  }
+
+  Widget iconButtonWidget(IconData icon, String text) {
+    return SizedBox(
+      width: double.infinity,
+      child: FlatButton.icon(
+        icon: Icon(icon, size: 15,),
+        color: Colors.white.withOpacity(0.2),
+        label: Text(
+          text,
+          style: TextStyle(
+            fontSize: 12,
+            letterSpacing: 1.5,
+          ),
+        ),
+        onPressed: () {},
+      ),
+    );
   }
 }
